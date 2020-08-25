b3f80e09b4f58a261e735fde2f91dac271cf2ac9
andyang <yang.bng86@gmail.com>
2020-08-25 22:36:50 +0800

    staggered layout manager

--- app/src/main/java/tw/andyang/kotlinandroidworkshop/MainActivity.kt
+++ app/src/main/java/tw/andyang/kotlinandroidworkshop/MainActivity.kt
@@ -2,8 +2,8 @@ package tw.andyang.kotlinandroidworkshop
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,13 +14,18 @@ class MainActivity : AppCompatActivity() {
 
         val adapter = TodoAdapter()
         recyclerView.adapter = adapter
-        recyclerView.layoutManager = GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false)
-        recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        recyclerView.layoutManager = StaggeredGridLayoutManager( 3, GridLayoutManager.VERTICAL)
 
         adapter.refresh(
             listOf(
-                Todo("hello", false),
+                Todo("hello world! hello world! hello world! hello world! hello world!", false),
                 Todo("world", false),
+                Todo("hello world! hello world! hello world! hello world! hello world!", false),
+                Todo("hello", false),
+                Todo("hello", false),
+                Todo("hello world! hello world! hello world! hello world! hello world!", false),
+                Todo("hello", false),
+                Todo("hello", false),
                 Todo("hello", false),
                 Todo("world", false)
             )
