2f84cc57dd7c028d39b1511bf1ca944356f1c9fa
Cateyes <lovelycateyes@gmail.com>
2020-09-22 23:50:42 +0800

    增加 Converter

--- app/src/main/java/tw/andyang/kotlinandroidworkshop/database/AppDatabase.kt
+++ app/src/main/java/tw/andyang/kotlinandroidworkshop/database/AppDatabase.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(
     version = 1,
@@ -12,6 +13,7 @@ import androidx.room.RoomDatabase
     ],
     exportSchema = false
 )
+@TypeConverters(Converters::class)
 abstract class AppDatabase: RoomDatabase() {
     companion object {
         private const val DATABASE_NAME = "todo_list_db"
